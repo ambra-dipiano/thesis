dff32af9d4ff336664d2a9f19d56351b3b92953d
ambra-dipiano <ambra.dipiano@gmail.com>
2019-10-17 16:35:18 +0200

    [fix] script_sensitivity.py :: fixed max likelihood call, and tentative of producing sensitivity from only bkg

--- script_sensitivity.py
+++ script_sensitivity.py
@@ -7,20 +7,21 @@ from module_plot import *
 import pandas as pd
 
 path = '/home/ambra/Desktop/cluster-morgana/irf_degraded/'
+outpath = path + 'South_z20_average_100s/'
 model = '$CTOOLS/share/models/crab.xml'
+bkg = outpath + 'irf_bkg.xml'
 
 prod_n = 3
 caldb_nom = 'prod%db' %prod_n
 caldb_deg = 'degr%db' %prod_n
 irf = 'South_z20_average_100s'
 
-outpath = path + 'crab_test/'
 event_nom = outpath+'crab.fits'
 event_deg = outpath+'crab_degraded.fits'
 output_nom = outpath+'prod%db_sens.csv' %prod_n
 output_deg = outpath+'degr%db_sens.csv' %prod_n
 results_nom = outpath+'prod%db_results.xml' %prod_n
-results_deg = outpath+'degr%db_mlike.xml' %prod_n
+results_deg = outpath+'degr%db_results.xml' %prod_n
 
 # INITIALIZE ---!
 nObj = analysis('/config_irf.xml')
@@ -28,18 +29,19 @@ nObj.e = [0.03, 150.0]
 nObj.t = [0, 100]
 nObj.caldb = caldb_nom
 nObj.irf = irf
-nObj.model = model
+nObj.model = bkg
 # NOMINAL SIM ---!
-nObj.event = event_nom
-nObj.pointing = [83.63-1.475, 22.01-1.371]  # (deg)
+nObj.output = event_nom
+nObj.pointing = [83.63, 22.01]  # (deg)
 nObj.eventSim()
 # NOMINAL MAX LIKELIHOOD ---!
-nObj.event_selected = event_nom
-nObj.likeXml = results_nom
+nObj.input = event_nom
+nObj.output = results_nom
+nObj.maxLikelihood()
 # NOMINAL SENS ---!
-nObj.sensCsv = output_nom
-nObj.likeXml = model
-nObj.srcName = 'Crab'
+nObj.model = results_nom
+nObj.output = output_nom
+nObj.srcName = 'CTABackgroundModel'
 nObj.eventSens()
 print('crab nominal done')
 
@@ -49,19 +51,22 @@ dObj.e = [0.03, 150.0]
 dObj.t = [0, 100]
 dObj.caldb = caldb_nom
 dObj.irf = irf
+dObj.model = bkg
 # DEGRADE IRF ---!
 dObj.degradeIRF()
-dObj.caldb = caldb_deg
+dObj.irf = 'North_z20_average_100s'
 # DEGRADE SIM ---!
-dObj.event = event_deg
-dObj.pointing = [83.63-1.475, 22.01-1.371]  # (deg)
+dObj.output = event_deg
+dObj.pointing = [83.63, 22.01]  # (deg)
 dObj.eventSim()
 # NOMINAL MAX LIKELIHOOD ---!
-nObj.event_selected = event_deg
-nObj.likeXml = results_deg
+dObj.input = event_deg
+dObj.output = results_deg
+dObj.maxLikelihood()
 # DEGRADE SENS ---!
-dObj.sensCsv = output_deg
-dObj.srcName = 'Crab'
+dObj.model = results_deg
+dObj.output = output_deg
+dObj.srcName = 'CTABackgroundModel'
 dObj.eventSens()
 print('Crab degraded done')
 
@@ -84,10 +89,10 @@ flux_deg = np.array(df_deg[cols[4]])
 print(sens_nom.max(), sens_deg.max())
 print(sens_nom[5], sens_deg[5])
 
-showSensitivity([10**energy_nom, 10**energy_deg], [sens_nom, sens_deg], savefig1, marker=['+', 'x'],
-                xlabel='energy (TeV)', ylabel='E$^2$F sensitivity (erg/cm$^2$/s)',
-                label=['full sens irf', 'degraded irf'], fontsize=12)
-
-showSensitivity([10**energy_nom, 10**energy_deg], [flux_nom, flux_deg], savefig2, marker=['+', 'x'],
-                xlabel='energy (TeV)', ylabel='ph flux (ph/cm$^2$/s)',
-                label=['full sens irf', 'degraded irf'], fontsize=12)
+# showSensitivity([10**energy_nom, 10**energy_deg], [sens_nom, sens_deg], savefig1, marker=['+', 'x'],
+#                 xlabel='energy (TeV)', ylabel='E$^2$F sensitivity (erg/cm$^2$/s)',
+#                 label=['full sens irf', 'degraded irf'], fontsize=12)
+#
+# showSensitivity([10**energy_nom, 10**energy_deg], [flux_nom, flux_deg], savefig2, marker=['+', 'x'],
+#                 xlabel='energy (TeV)', ylabel='ph flux (ph/cm$^2$/s)',
+#                 label=['full sens irf', 'degraded irf'], fontsize=12)
