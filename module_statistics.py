00332d7e8211c3d8c6c823a9432fbfeeb7043505
ambra-dipiano <ambra.dipiano@gmail.com>
2019-11-28 18:31:59 +0100

    [new] module_statistics + wilks notebook :: chi2 reduced test and 5sigma thresholds

--- module_statistics.py
+++ module_statistics.py
@@ -8,6 +8,7 @@ import numpy as np
 from matplotlib.patches import Rectangle
 from scipy import stats
 from scipy.stats import rayleigh, norm, chi2
+import pandas as pd
 from matplotlib.colors import LogNorm
 from matplotlib.lines import Line2D
 from matplotlib.patches import Ellipse, Circle
@@ -22,14 +23,15 @@ extra2 = Line2D([0], [0], ls='-.', color='k', lw='1')
 
 
 # HIST 1D GAUSSIAN DISTRIBUTION ---!
-def hist1d_gauss(x, mean, loc=0, threshold=1, nbin=20, width=None, hist=True, fontsize=12, color='b', alpha=0.5, title='gaussian fit',
-                 ax_thresh=0.2, xlabel='x', ylabel='y', leglabel='data', filename='hist1d_gauss.png') :
+def hist1d_gauss(x, mean, loc=0, threshold=1, nbin=20, width=None, hist=True, fontsize=12, color='b',
+                 alpha=0.5, title='gaussian fit', ax_thresh=0.2, xlabel='x', ylabel='y', leglabel='data',
+                 filename='hist1d_gauss.png', show=False):
 
-  if width == None :
+  if width == None:
     width = threshold/nbin
-  if nbin == None :
+  if nbin == None:
     nbin = int(threshold/width)
-  if nbin == None and width == None :
+  if nbin == None and width == None:
     print('Error: set either nbin or width')
 
   fig = plt.figure()
@@ -54,19 +56,22 @@ def hist1d_gauss(x, mean, loc=0, threshold=1, nbin=20, width=None, hist=True, fo
 
   plt.tight_layout()
   fig.savefig(filename)
-
+  # show fig ---!
+  plt.show() if show == True else None
+  plt.close()
   return fig, ax
 
 
 # HIST 1D RAYLEIGH DISTRIBUTION ---!
-def hist1d_rayleigh(x, mean, rayleigh_prms={'loc':0, 'scale':[1]}, threshold=1, nbin=None, width=None, hist=True, fontsize=12, color='b', alpha=0.5, title='rayleigh fit',
-                    ax_thresh=0.2, xlabel='x', ylabel='y', leglabel='data', filename='hist1d_rayleigh.png') :
+def hist1d_rayleigh(x, mean, rayleigh_prms={'loc':0, 'scale':[1]}, threshold=1, nbin=None, width=None, hist=True,
+                    fontsize=12, color='b', alpha=0.5, title='rayleigh fit', ax_thresh=0.2, xlabel='x', ylabel='y',
+                    leglabel='data', filename='hist1d_rayleigh.png', show=False):
 
-  if width == None :
+  if width == None:
     width = threshold/nbin
-  if nbin == None :
+  if nbin == None:
     nbin = int(threshold/width)
-  if nbin == None and width == None :
+  if nbin == None and width == None:
     print('Error: set either nbin or width')
 
   fig = plt.figure()
@@ -92,13 +97,16 @@ def hist1d_rayleigh(x, mean, rayleigh_prms={'loc':0, 'scale':[1]}, threshold=1, 
 
   plt.tight_layout()
   fig.savefig(filename)
-
+  # show fig ---!
+  plt.show() if show == True else None
+  plt.close()
   return fig, ax
 
 
 # RAYLEIGH CDF WITH CONFIDENCE INTERVAL ---!
-def rayleigh_cdf(x, loc=0, scale=1, if_CI=True, probs=(0.6827, 0.9545, 0.9973, 0.99994), xlabel='x', title='x$\\sim$ RA($\\gamma$) CDF',
-                 colors=('k', 'r', 'orange', 'm'), fontsize=12, filename='theo_rayleigh_cdf.png') :
+def rayleigh_cdf(x, loc=0, scale=1, if_CI=True, probs=(0.6827, 0.9545, 0.9973, 0.99994),
+                 xlabel='x', title='x$\\sim$ RA($\\gamma$) CDF', colors=('k', 'r', 'orange', 'm'),
+                 fontsize=12, filename='theo_rayleigh_cdf.png', show=False):
 
   fig = plt.figure()
   plt.rc('text', usetex=True)
@@ -109,9 +117,9 @@ def rayleigh_cdf(x, loc=0, scale=1, if_CI=True, probs=(0.6827, 0.9545, 0.9973, 0
   ax.axvline(scale, c='maroon', label='$\gamma$')
   ax.axvline(np.std(x), c='maroon', ls=':', label='1 std =%.2f' %(np.std(x)))
 
-  if if_CI is True :
+  if if_CI is True:
     x_critical = []
-    for i in range(len(probs)) :
+    for i in range(len(probs)):
       x_critical.append(stats.rayleigh.ppf(q=probs[i], loc=loc, scale=scale))
       ax.axvline(x_critical[i], c=colors[i], ls='-.',
                  label='x=%.2f, %.2f' %(x_critical[i],probs[i]*100)+'%')
@@ -125,13 +133,16 @@ def rayleigh_cdf(x, loc=0, scale=1, if_CI=True, probs=(0.6827, 0.9545, 0.9973, 0
 
   plt.tight_layout()
   fig.savefig(filename)
-
+  # show fig ---!
+  plt.show() if show == True else None
+  plt.close()
   return fig, ax
 
 
 # RAYLEIGH PDF WITH CONFIDENCE INTERVAL ---!
-def rayleigh_pdf(x, loc=0, scale=1, if_CI=True, probs=(0.6827, 0.9545, 0.9973, 0.99994), xlabel='x', title='x$\\sim$ RA($\\gamma$) CDF',
-                 colors=('k', 'r', 'orange', 'm'), fontsize=12, filename='theo_rayleigh_cdf.png') :
+def rayleigh_pdf(x, loc=0, scale=1, if_CI=True, probs=(0.6827, 0.9545, 0.9973, 0.99994),
+                 xlabel='x', title='x$\\sim$ RA($\\gamma$) CDF', colors=('k', 'r', 'orange', 'm'),
+                 fontsize=12, filename='theo_rayleigh_cdf.png', show=False):
 
   fig = plt.figure()
   plt.rc('text', usetex=True)
@@ -142,9 +153,9 @@ def rayleigh_pdf(x, loc=0, scale=1, if_CI=True, probs=(0.6827, 0.9545, 0.9973, 0
   ax.axvline(scale, c='maroon', label='$\gamma$')
   ax.axvline(np.std(x), c='maroon', ls=':', label='1 std =%.2f' %(np.std(x)))
 
-  if if_CI is True :
+  if if_CI is True:
     x_critical = []
-    for i in range(len(probs)) :
+    for i in range(len(probs)):
       x_critical.append(stats.rayleigh.ppf(q=probs[i], loc=loc, scale=scale))
       ax.axvline(x_critical[i], c=colors[i], ls='-.',
                  label='x=%.2f, %.2f' %(x_critical[i],probs[i]*100)+'%')
@@ -158,21 +169,23 @@ def rayleigh_pdf(x, loc=0, scale=1, if_CI=True, probs=(0.6827, 0.9545, 0.9973, 0
 
   plt.tight_layout()
   fig.savefig(filename)
-
+  # show fig ---!
+  plt.show() if show == True else None
+  plt.close()
   return fig, ax
 
 
 # 2D HISTOGRAM WITH RAYLEIGH CONFIDENCE INTERVAL ---!
-def hist2d_rayleigh_CI(x, y, nbin=None, width=None, rayleigh_prms={'loc':0, 'scale':1}, xcentre=0, ycentre=0, threshold=1, probs=(0.6827, 0.9545, 0.9973, 0.99994), colors=('k', 'r', 'orange', 'm'), ax_thresh=0.2, xlabel='x', ylabel='y', title='confidence intervals from theoretical distribution', fontsize=12, filename='hist2d_CIrayleigh.png') :
+def hist2d_rayleigh_CI(x, y, nbin=None, width=None, rayleigh_prms={'loc':0, 'scale':1}, xcentre=0, ycentre=0, threshold=1, probs=(0.6827, 0.9545, 0.9973, 0.99994), colors=('k', 'r', 'orange', 'm'), ax_thresh=0.2, xlabel='x', ylabel='y', title='confidence intervals from theoretical distribution', fontsize=12, filename='hist2d_CIrayleigh.png', show=False):
 
   xmean = np.mean(x)
   ymean = np.mean(y)
 
-  if width is None :
+  if width is None:
     width = threshold/nbin
-  if nbin is None :
+  if nbin is None:
     nbin = int(threshold/width)
-  if nbin is None and width is None :
+  if nbin is None and width is None:
     print('Error: set either nbin or width')
 
   fig = plt.figure()
@@ -204,7 +217,9 @@ def hist2d_rayleigh_CI(x, y, nbin=None, width=None, rayleigh_prms={'loc':0, 'sca
 
   plt.tight_layout()
   fig.savefig(filename)
-
+  # show fig ---!
+  plt.show() if show == True else None
+  plt.close()
   return fig, ax
 
 
@@ -218,17 +233,17 @@ def eigsorted(cov):
 
 # 2D HISTOGRAM WITH GAUSSIAN COVARIANCE CONFIDENCE INTERVAL ---!
 def hist2d_gauss_CI(x, y, nbin=None, width=None, xcentre=0, ycentre=0, threshold=1, nstd=(1, 2, 3, 5),
-                    colors=('k', 'r', 'orange', 'm'), ax_thresh=0.2, xlabel='x', ylabel='y',
-                    title='confidence intervals from theoretical distribution', fontsize=12, filename='hist2d_CIgauss.png') :
+                    colors=('k', 'r', 'orange', 'm'), ax_thresh=0.2, xlabel='x', ylabel='y', show=False,
+                    title='confidence intervals from theoretical distribution', fontsize=12, filename='hist2d_CIgauss.png'):
 
   xmean = np.mean(x)
   ymean = np.mean(y)
 
-  if width is None :
+  if width is None:
     width = threshold/nbin
-  if nbin is None :
+  if nbin is None:
     nbin = int(threshold/width)
-  if nbin is None and width is None :
+  if nbin is None and width is None:
     print('Error: set either nbin or width')
 
   fig = plt.figure()
@@ -261,18 +276,21 @@ def hist2d_gauss_CI(x, y, nbin=None, width=None, xcentre=0, ycentre=0, threshold
 
   plt.tight_layout()
   fig.savefig(filename)
-
+  # show fig ---!
+  plt.show() if show == True else None
+  plt.close()
   return fig, ax
 
 
 # 2D HISTOGRAM MAP ---!
 def hist2d_map(x, y, trials, nbin=None, width=None, xcentre=0, ycentre=0, threshold=1, ax_thresh=0.2, xlabel='x', ylabel='y',
                title='probability map', fontsize=12, filename='hist2d_map.png', if_CI=None, rayleigh={'loc':0, 'scale':1},
-               nstd=(1, 2, 3, 5), colors=('k', 'r', 'orange', 'm'), probs=(0.6827, 0.9545, 0.9973, 0.99994), smooth=True) :
+               nstd=(1, 2, 3, 5), colors=('k', 'r', 'orange', 'm'), probs=(0.6827, 0.9545, 0.9973, 0.99994),
+               smooth=True, show=False):
 
-  if width is None :
+  if width is None:
     width = threshold/nbin
-  if nbin is None :
+  if nbin is None:
     nbin = int(threshold/width)
   if nbin is None and width is None:
     print('Error: set either nbin or width')
@@ -298,10 +316,10 @@ def hist2d_map(x, y, trials, nbin=None, width=None, xcentre=0, ycentre=0, thresh
 
   plt.scatter(xcentre, ycentre, c='w', marker='*', s=1e2)
 
-  if if_CI is None :
+  if if_CI is None:
     pass
 
-  elif if_CI.lower() is 'rayleigh' :
+  elif if_CI.lower() is 'rayleigh':
     xmean = np.mean(x)
     ymean = np.mean(y)
     plt.plot([], [], c='none', label='Reyleigh')
@@ -314,7 +332,7 @@ def hist2d_map(x, y, trials, nbin=None, width=None, xcentre=0, ycentre=0, thresh
       cir.set_facecolor('none')
       ax.add_artist(cir)
 
-  elif if_CI.lower() is 'gauss' or if_CI.lower is 'covariance' or if_CI.lower is 'cov' :
+  elif if_CI.lower() is 'gauss' or if_CI.lower is 'covariance' or if_CI.lower is 'cov':
     xmean = np.mean(x)
     ymean = np.mean(y)
     plt.plot([], [], c='none', label='gauss')
@@ -329,7 +347,7 @@ def hist2d_map(x, y, trials, nbin=None, width=None, xcentre=0, ycentre=0, thresh
                     angle=theta, color=colors[i], lw=2)
       ell.set_facecolor('none')
       ax.add_artist(ell)
-  else :
+  else:
     print('Error: if_CI parameter value not understood')
 
   m = plt.cm.ScalarMappable(cmap='jet')
@@ -345,18 +363,20 @@ def hist2d_map(x, y, trials, nbin=None, width=None, xcentre=0, ycentre=0, thresh
 
   plt.tight_layout()
   fig.savefig(filename)
-
+  # show fig ---!
+  plt.show() if show == True else None
+  plt.close()
   return fig, ax
 
 
 # WILKS THEOREM DIST FOR EMPTY FIELDS ---!
-def ts_wilks(x, trials, df=1, nbin=None, width=None, ylim=None, xlim=None, xlabel='TS',
-             fontsize=12, figsize=(4,6),
-             title='TS distribution (empty fields)', filename='wilks_preTrials.png') :
+def ts_wilks(x, trials, df=1, nbin=None, width=None, ylim=None, xlim=None, show=False,
+             fontsize=12, figsize=(4,6), xlabel='TS', ylabel='normalised counts',
+             title='TS distribution (empty fields)', filename='wilks_preTrials.png'):
 
-  if width is None :
+  if width is None:
     width = (max(x)-min(x))/nbin
-  if nbin is None :
+  if nbin is None:
     nbin = int((max(x)-min(x))/width)
   if nbin is None and width is None:
     print('Error: set either nbin or width')
@@ -367,69 +387,18 @@ def ts_wilks(x, trials, df=1, nbin=None, width=None, ylim=None, xlim=None, xlabe
 
   ax = plt.subplot(111, yscale='log')
   h, edges = np.histogram(x, bins=int(nbin), density=False, range=(0., max(x)))
-  h_norm = h / trials
-  cbin = (edges[1:] + edges[:-1]) / 2
-  yerr = np.sqrt(h) / trials
-  xerr = (edges[:-1] - edges[1:]) / 2
-
-  x2 = []
-  for i in range(len(x)):
-    if x[i] > edges[1] :
-      x2.append(x[i])
-
-  plt.errorbar(cbin, h_norm, fmt='k+', yerr=yerr, xerr=xerr, markersize=5, label='data')
-  plt.plot(x2, stats.chi2.pdf(x2, df=df) / 2, lw=1, ls='--', label='$\\chi^2$/2')
-
-  plt.xlabel(xlabel, fontsize=fontsize)
-  plt.ylabel('normalized counts', fontsize=fontsize)
-  plt.title(title, fontsize=fontsize)
-  plt.legend(loc=0, fontsize=fontsize)
-  plt.xlim(xlim) if xlim is not None else None
-  plt.ylim(ylim) if ylim is not None else None
-
-  plt.tight_layout()
-  fig.savefig(filename)
-
-  return fig, ax
-
-
-# WILKS THEOREM P-VALUES FOR EMPTY FIELDS ---!
-def p_values(x, trials, df=1, nbin=None, width=None, ylim=None, xlim=None, xlabel='h',
-             fontsize=12, figsize=(4,6),
-             title='p-value (empty fields)', filename='pvalue_preTrials.png') :
-
-  if width is None :
-    width = (max(x)-min(x))/nbin
-  if nbin is None :
-    nbin = int((max(x)-min(x))/width)
-  if nbin is None and width is None:
-    print('Error: set either nbin or width')
-
-  fig = plt.figure(figsize=figsize)
-  plt.rc('text', usetex=True)
-  sns.set()
-
-  ax = plt.subplot(111, yscale='log')
-
-  h, edges = np.histogram(x, bins=int(nbin), density=False, range=(0., max(x)))
-  yerr = np.sqrt(h) / trials
+  yerr = np.sqrt(h)/trials
   h = h/trials
   cbin = (edges[1:] + edges[:-1]) / 2
-  p = (1 - stats.chi2.cdf(cbin, df=df))
-  #    yerr = np.sqrt(p) / trials
   xerr = (edges[:-1] - edges[1:]) / 2
 
-  x2 = []
-  for i in range(len(x)):
-    if x[i] > edges[1] :
-      x2.append(x[i])
-
-  plt.errorbar(cbin[0], h[0], yerr=yerr[0], xerr=xerr[0], fmt='k+', markersize=5)
-  plt.errorbar(cbin[1:], p[1:], yerr=yerr[1:], xerr=xerr[1:], fmt='k+', markersize=5, label='p-values')
-  plt.plot(x2, (1 - stats.chi2.cdf(x2, df=1)), lw=1, ls='-.', c='maroon', label='$P$')
+  x2 = np.arange(0, 30, 1)
+  plt.errorbar(cbin, h, fmt='k+', yerr=yerr, xerr=xerr, markersize=5, label='ts')
+  plt.plot(x2, stats.chi2.pdf(x2, df=df), c='orange', lw=1, ls='--', label='$\\chi^2$(dof=%d)' %df)
+  plt.plot(x2, stats.chi2.pdf(x2, df=df)/2, lw=1, ls='--', label='$\\chi^2$/2(dof=%d)' %df)
 
   plt.xlabel(xlabel, fontsize=fontsize)
-  plt.ylabel('probability', fontsize=fontsize)
+  plt.ylabel(ylabel, fontsize=fontsize)
   plt.title(title, fontsize=fontsize)
   plt.legend(loc=0, fontsize=fontsize)
   plt.xlim(xlim) if xlim is not None else None
@@ -438,4 +407,131 @@ def p_values(x, trials, df=1, nbin=None, width=None, ylim=None, xlim=None, xlabe
   plt.tight_layout()
   fig.savefig(filename)
 
+  # show fig ---!
+  plt.show() if show == True else None
+  plt.close()
+
   return fig, ax
+
+# WILKS THEOREM P-VALUES FOR EMPTY FIELDS ---!
+def p_values(x, trials, df=1, nbin=None, width=None, ylim=None, xlim=None, show=False,
+             fontsize=12, figsize=(4,6), xlabel='h', ylabel='p-values',
+             title='p-value (empty fields)', filename='pvalue_preTrials.png'):
+
+  if width is None:
+    width = (max(x)-min(x))/nbin
+  if nbin is None:
+    nbin = int((max(x)-min(x))/width)
+  if nbin is None and width is None:
+    print('Error: set either nbin or width')
+
+  fig = plt.figure(figsize=figsize)
+  plt.rc('text', usetex=True)
+  sns.set()
+
+  ax = plt.subplot(111, yscale='log')
+
+  h, edges = np.histogram(x, bins=int(nbin), density=False, range=(0., max(x)))
+  yerr = np.sqrt(h)/trials
+  h = h/trials
+  cbin = (edges[1:] + edges[:-1]) / 2
+  p = (1 - stats.chi2.cdf(cbin, df=df)) / 2
+  xerr = (edges[:-1] - edges[1:]) / 2
+
+  x2 = np.arange(0, 30, 1)
+  plt.errorbar(cbin[0], p[0], yerr=yerr[0], xerr=xerr[0], fmt='k+', markersize=5)
+  plt.errorbar(cbin[1:], p[1:], yerr=yerr[1:], xerr=xerr[1:], fmt='k+', markersize=5, label='ts')
+  plt.plot(x2, (1 - stats.chi2.cdf(x2, df=df)), lw=1, ls='-.', c='green', label='$\\chi^2$(dof=%d)' %df)
+  plt.plot(x2, (1 - stats.chi2.cdf(x2, df=df))/2, lw=1, ls='-.', c='maroon', label='$\\chi^2$/2(dof=%d)' %df)
+  # plt.legend(('$\\chi^2$/2(dof=%d)', '$\\chi^2$(dof=%d)', 'ts'), loc=0, fontsize=fontsize)
+  plt.axhline(3e-7, c='gray', ls=':', alpha=0.5)
+  plt.text(23, 2e-7, '5$\sigma$', fontsize=fontsize, alpha=0.5)
+
+  plt.xlabel(xlabel, fontsize=fontsize)
+  plt.ylabel(ylabel, fontsize=fontsize)
+  plt.title(title, fontsize=fontsize)
+  plt.legend(loc=0, fontsize=fontsize)
+  plt.xlim(xlim) if xlim is not None else None
+  plt.ylim(ylim) if ylim is not None else None
+
+  plt.tight_layout()
+  fig.savefig(filename)
+
+  # show fig ---!
+  plt.show() if show == True else None
+  plt.close()
+
+  return fig, ax
+
+# WILKS THEOREM P-VALUES FOR EMPTY FIELDS ---!
+def ts_wilks_cumulative(x, trials, df=1, nbin=None, width=None, ylim=None, xlim=None, show=False,
+                        fontsize=12, figsize=(6,4), xlabel='h', ylabel='cumulative probability',
+                        title='p-value (empty fields)', filename='cumulative_preTrials.png'):
+
+  if width is None:
+    width = (max(x)-min(x))/nbin
+  if nbin is None:
+    nbin = int((max(x)-min(x))/width)
+  if nbin is None and width is None:
+    print('Error: set either nbin or width')
+
+  fig = plt.figure(figsize=figsize)
+  plt.rc('text', usetex=True)
+  sns.set()
+
+  ax = plt.subplot(111)
+
+  h, edges = np.histogram(x, bins=int(nbin), density=False, range=(0., max(x)))
+  yerr = np.sqrt(h)/trials
+  h = h/trials
+  cbin = (edges[1:] + edges[:-1]) / 2
+  p = stats.chi2.cdf(cbin, df=df)
+  xerr = (edges[:-1] - edges[1:]) / 2
+
+  x2 = np.arange(0, 30, 1)
+  plt.errorbar(cbin[0], p[0], yerr=yerr[0], xerr=xerr[0], fmt='k+', markersize=5)
+  plt.errorbar(cbin[1:], p[1:], yerr=yerr[1:], xerr=xerr[1:], fmt='k+', markersize=5, label='ts')
+  plt.plot(x2, stats.chi2.cdf(x2, df=df), lw=1, ls='-.', c='maroon', label='$P$(dof=%d)' %df)
+  # plt.legend(('$\\chi^2$/2(dof=%d)', '$\\chi^2$(dof=%d)', 'ts'), loc=0, fontsize=fontsize)
+  plt.axhline(1-3e-7, c='gray', ls=':', alpha=0.5)
+  plt.text(1, 0.95, '5$\sigma$', fontsize=fontsize, alpha=0.5)
+
+  plt.xlabel(xlabel, fontsize=fontsize)
+  plt.ylabel(ylabel, fontsize=fontsize)
+  plt.title(title, fontsize=fontsize)
+  plt.legend(loc=0, fontsize=fontsize)
+  plt.xlim(xlim) if xlim is not None else None
+  plt.ylim(ylim) if ylim is not None else None
+
+  plt.tight_layout()
+  fig.savefig(filename)
+
+  # show fig ---!
+  plt.show() if show == True else None
+  plt.close()
+
+  return fig, ax
+
+def chi2_reduced(x, trials, df=1, nbin=None, width=None, var=True):
+
+  if width is None:
+    width = (max(x)-min(x))/nbin
+  if nbin is None:
+    nbin = int((max(x)-min(x))/width)
+  if nbin is None and width is None:
+    print('Error: set either nbin or width')
+
+  h, edges = np.histogram(x, bins=int(nbin), density=False, range=(0., max(x)))
+  yerr = np.sqrt(h)/trials
+  h = h/trials
+  # yerr = np.sqrt(h)
+  cbin = (edges[1:] + edges[:-1])/2
+  p = (1 - stats.chi2.pdf(cbin, df=df))/2
+
+  if var:
+    chi2 = 2*np.sum((h[1:] - p[1:])**2/h[1:])
+  else:
+    chi2 = 2*np.sum((h[1:] - p[1:])**2/p[1:])
+  chi2r = chi2 / (len(h[1:]) - 1)
+
+  return chi2, chi2r
