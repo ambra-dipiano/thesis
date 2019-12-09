5fa313f05408d7ec99374b88c58ad9d97e43aff3
ambra-dipiano <ambra.dipiano@gmail.com>
2019-12-09 11:33:42 +0100

    [up] pkg*.py :: __splitEvents() can split one phlist into multiple smaller ones

--- pkg_blindsearch.py
+++ pkg_blindsearch.py
@@ -493,13 +493,83 @@ class Analysis() :
     xml.save(self.output)
     return
 
+  # from single fits file produces subfiles ---!
+  def __splitEvents(self, max_length):
+    with fits.open(self.output) as hdul:
+      h1 = hdul[1].header
+      h2 = hdul[2].header
+      all_data = hdul[1].data
+      times = hdul[1].data.field('TIME')
+      n = 0
+      ext2 = hdul[2].data
+      GTIi = hdul[2].data[0][0]
+      GTIid = 0
+      print(len(times))
+      for i, t in enumerate(times):
+        if (t > (max_length*(n+1)) or t == (max_length*(n+1))):
+          print(i)
+          print(n, max_length*(n+1))
+          n += 1
+          ext1 = all_data[GTIid:i]  # all untill index i-1 which is the last < or = to max_length ---!
+          GTIf = times[i-1]
+          # create output FITS file empty ---!
+          hdu_new = fits.PrimaryHDU()
+          hdul_new = fits.HDUList([hdu_new])
+          filename = self.output.replace('.fits', '_n%03d.fits' %(n))
+          hdul_new.writeto(filename)
+          with fits.open(filename, mode='update') as hdul_new:
+            hdu1 = fits.BinTableHDU(name='EVENTS', data=ext1, header=h1)
+            hdu2 = fits.BinTableHDU(name='GTI', data=ext2, header=h2)
+            hdul_new.append(hdu1)
+            hdul_new.append(hdu2)
+            hdul_new.writeto(filename, overwrite=True)
+          # update ---!
+          with fits.open(filename, mode='update') as hdul_new:
+            # modify indexes ---!
+            indexes = hdul_new[1].data.field(0)
+            for i, ind in enumerate(indexes):
+              hdul_new[1].data.field(0)[i] = i + 1
+            # modify GTI ---!
+            hdul_new[2].data[0][0] = GTIi
+            hdul_new[2].data[0][1] = GTIf
+            hdul_new.flush()
+          GTIi = GTIf
+          GTIid = i
+        elif i == len(times)-1:
+          n += 1
+          ext1 = all_data[GTIid:]  # all index from last chunk till the end ---!
+          GTIf = times[-1]
+          print(i)
+          print(GTIi, GTIf)
+          # create output FITS file empty ---!
+          hdu_new = fits.PrimaryHDU()
+          hdul_new = fits.HDUList([hdu_new])
+          filename = self.output.replace('.fits', '_n%03d.fits' %(n))
+          hdul_new.writeto(filename)
+          with fits.open(filename, mode='update') as hdul_new:
+            hdu1 = fits.BinTableHDU(name='EVENTS', data=ext1, header=h1)
+            hdu2 = fits.BinTableHDU(name='GTI', data=ext2, header=h2)
+            hdul_new.append(hdu1)
+            hdul_new.append(hdu2)
+            hdul_new.writeto(filename, overwrite=True)
+          # update ---!
+          with fits.open(filename, mode='update') as hdul_new:
+            # modify indexes ---!
+            indexes = hdul_new[1].data.field(0)
+            for i, ind in enumerate(indexes):
+              hdul_new[1].data.field(0)[i] = i + 1
+            # modify GTI ---!
+            hdul_new[2].data[0][0] = GTIi
+            hdul_new[2].data[0][1] = GTIf
+            hdul_new.flush()
+    return
+
   # created one FITS table containing all events and GTIs ---!
-  def appendEvents(self):
+  def appendEvents(self, max_length=None):
     # remove old ---!
     if os.path.isfile(self.output):
       os.remove(self.output)
     # collect events ---!
-    print(len(self.input))
     for i, f in enumerate(self.input):
       with fits.open(f) as hdul:
         if i==0:
@@ -527,9 +597,13 @@ class Analysis() :
       indexes = hdul[1].data.field(0)
       for i, ind in enumerate(indexes):
         hdul[1].data.field(0)[i] = i+1
-      hdul.flush()
       # modify GTI ---!
       hdul[2].data[0][1] = GTIlast
+      hdul.flush()
+
+    if max_length != None:
+      self.__splitEvents(max_length=max_length)
+
     return
 
   # ctselect wrapper ---!
