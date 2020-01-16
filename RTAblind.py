79bf94ff59f9f42719d9feb402719828a75db5ea
ambra-dipiano <ambra.dipiano@gmail.com>
2020-01-16 12:00:51 +0100

    [use] RTAblind.py

--- RTAblind.py
+++ RTAblind.py
@@ -29,7 +29,7 @@ texp = [10, 100]  # exposure times (s)
 texp.sort()
 tint = len(texp)
 tdelay = 30  # slewing time (s)
-tmin = [90, 30]  # start of bin to select (s)
+tmin = [90, 50]  # start of bin to select (s)
 tmax = []  # end of bin to select (s)
 for i in range(tint):
   tmax.append(tmin[i] + texp[i])
@@ -41,18 +41,18 @@ roi = 5  # region of interest for simulation and selection (deg)
 wbin = 0.02  # skymap bin width (deg)
 corr_rad = 0.1  # Gaussian
 confidence = (0.68, 0.95, 0.9973)  # confidence interval for asymmetrical errors (%)
-max_src = 10  # max candidates
-ts_threshold = 9  # TS threshold for reliable detection
+max_src = 5  # max candidates
+ts_threshold = 25  # TS threshold for reliable detection
 reduce_flux = None  # flux will be devided by factor reduce_flux, if nominal then set to None ---!
 
 # conditions control ---!
 checks = False  # prints checks info ---!
-if_ebl = False  # uses the EBL absorbed template ---!
+if_ebl = True  # uses the EBL absorbed template ---!
 if_cut = False  # adds a cut-off parameter to the source model ---!
 ebl_fits = False  # generate the EBL absorbed template ---!
 extract_spec = True  # generates spectral tables and obs definition models ---!
 irf_degrade = False  # use degraded irf ---!
-src_sort = False  # sorts scandidates from highest TS to lowest ---!
+src_sort = True  # sorts scandidates from highest TS to lowest ---!
 skip_exist = False  # skip trial if already existing in data file ---!
 debug = False  # prints logfiles on terminal ---!
 if_log = True  # saves logfiles ---!
@@ -332,7 +332,7 @@ for k in range(trials):
 
     # --------------------------------- RESULTS TABLE (csv) --------------------------------- !!!
 
-    header = '#trial,texp,sigma,Ndet,Nsrc,RA_det,DEC_det,RA_fit,DEC_fit,flux_ph,flux_erg,TS\n'
+    header = '#trial,texp,sigma,Ndet,Nsrc,RA_det,DEC_det,RA_fit,DEC_fit,flux_ph,TS\n'
     ID = 'ID%06d' % count
     csvName = p.getCsvDir() + fileroot + '%ds_chunk%02d.csv' % (texp[i], chunk)
 
